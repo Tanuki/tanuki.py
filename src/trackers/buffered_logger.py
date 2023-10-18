e1ed3c7828e1ae6228d9637ca01f14a710613094
Mart <mart@paperplane.ai>
2023-10-18 14:19:37 +0100

    updated buffered logger to have multiple configs and get initial dataset lengths

--- src/trackers/buffered_logger.py
+++ src/trackers/buffered_logger.py
@@ -22,8 +22,10 @@ class BufferedLogger(Logger):
         self.mapped_files = {}
         self.miss_count = 0
         self.hit_count = 0
+        self.flush_limit = {}
         self.log_directory = self._get_log_directory()
         self.bloom_filter = BloomFilter(*optimal_bloom_filter_params(EXPECTED_ITEMS, FALSE_POSITIVE_RATE))
+        self.configs = {}
         try:
             self.bloom_filter.load(self.log_directory)
         except FileNotFoundError:
@@ -31,7 +33,7 @@ class BufferedLogger(Logger):
 
         self.write_count = 0
         self.write_limit = 1000  # Save the Bloom filter every 1000 writes
-
+        self._get_dataset_sizes()
         super().__init__(self, level)
 
     def _get_log_directory(self):
@@ -54,6 +56,19 @@ class BufferedLogger(Logger):
 
         # 4. Write to where the code is being executed from.
         return os.path.join(os.getcwd(), ALIGN_FILE_NAME)
+
+    def _get_dataset_sizes(self):
+        # get all the files in the log directory
+        files = os.listdir(self.log_directory)
+        # discard all .json files
+        files = [x for x in files if ".json" not in x]
+        self.dataset_lengths = {}
+        for file in files:
+            with open(os.path.join(self.log_directory, file), "rb") as f:
+                dataset = f.read().decode('utf-8')
+            # get the total nr of /n in the file
+            log_file_path = os.path.join(self.log_directory, file)
+            self.dataset_lengths[log_file_path] = dataset.count("\n") - dataset.count("\\n") 
 
     def log_align(self, message, *args, **kws):
         log_directory = self._get_log_directory()
@@ -91,6 +106,7 @@ class BufferedLogger(Logger):
 
         if log_file_path not in self.buffers:
             self.buffers[log_file_path] = bytearray()
+            self.flush_limit[log_file_path] = 1
 
         self.buffers[log_file_path].extend(example_data)
 
@@ -101,11 +117,12 @@ class BufferedLogger(Logger):
             self.save_bloom_filter()
             self.write_count = 0  # Reset counter
 
-        if len(self.buffers[log_file_path]) >= 4096:  # Flush after reaching 4KB
+        if len(self.buffers[log_file_path]) >= self.flush_limit[log_file_path]:  # Flush after reaching 4KB
             with open(log_file_path, "a+b") as f:
                 f.write(self.buffers[log_file_path])
-
+            self.dataset_lengths[log_file_path] += 1
             self.buffers[log_file_path].clear()
+            self.flush_limit[log_file_path] = 2 * self.flush_limit[log_file_path]
         return True
 
     def save_bloom_filter(self):
@@ -116,21 +133,20 @@ class BufferedLogger(Logger):
             if len(buffer) > 0:
                 with open(log_file_path, "a+b") as f:
                     f.write(buffer)
+                self.dataset_lengths[log_file_path] = len(buffer)
                 buffer.clear()
 
-    def get_config(self, message):
+    def get_configs(self, log_file_path):
 
         """
         Get the config file for the function. Uses the message and log directory
         Config file has to have to be in .json
         """
 
-        log_directory = self._get_log_directory()
-        if not os.path.exists(log_directory):
-            os.makedirs(log_directory)
-        config_path = os.path.join(log_directory, f"{message}.json")
+        
+        config_path = f"{log_file_path}.json"
         if not os.path.exists(config_path):
-            self.config = {"current_model": "gpt-4", 
+            self.configs[log_file_path] = {"current_model": "gpt-4", 
                     "current_model_stats": {"trained_on_datapoints": 0,
                     "running_faults": []}, 
                     "last_training_run": {"trained_on_datapoints": 0}, 
@@ -139,18 +155,27 @@ class BufferedLogger(Logger):
                     "teacher_models" : ["gpt-4"],
                     "nr_of_training_runs": 0}
             with open(config_path, "w") as f:
-                json.dump(self.config, f)
+                json.dump(self.configs[log_file_path], f)
         else:
             with open(config_path, "r") as f:
-                self.config = json.load(f)
+                self.configs[log_file_path] = json.load(f)
+        
 
 
     def get_model(self, message):
         """
         Return the current model from the config file
         """
-        self.get_config(message)
-        return self.config["current_model"]
+        log_directory = self._get_log_directory()
+        log_file_path = os.path.join(log_directory, message)
+
+        if not os.path.exists(log_directory):
+            os.makedirs(log_directory)
+
+
+        self.get_configs(log_file_path)
+
+        return self.configs[log_file_path]["current_model"]
     
     def postprocess_datapoint(self, message, function_description, example, log = True):
         """
@@ -161,24 +186,21 @@ class BufferedLogger(Logger):
             ???
         """
 
-        if not isinstance(message, str):
-            self.message = str(message)
-        else:
-            self.message = message
-
         try:
-            if log or self.config["current_model"] in self.config["teacher_models"]:
+            log_directory = self._get_log_directory()
+            log_file_path = os.path.join(log_directory, message)
+            if log or self.configs[log_file_path]["current_model"] in self.configs[log_file_path]["teacher_models"]:
                 added = self.log_patch(message, example)
                 if added:
-                    self._update_datapoint_config(log)
+                    self._update_datapoint_config(log, log_file_path)
         except Exception as e:
             print(e)
             print("Could not add datapoint to training data")
             return None
         
-        self.check_for_finetuning(function_description)
+        self.check_for_finetuning(function_description, log_file_path)
     
-    def check_for_finetuning(self, function_description):
+    def check_for_finetuning(self, function_description, log_file_path):
         """
         Check for finetuning status
         If already finetuning, check for finetuning status
@@ -186,38 +208,40 @@ class BufferedLogger(Logger):
         """
         try:
             # check if already finetuning
-            if "job_id" in self.config["current_training_run"]:
+            if "job_id" in self.configs[log_file_path]["current_training_run"]:
                 # check for job status
-                self._check_finetuning_status()
+                self._check_finetuning_status(log_file_path)
             else:
             # check for finetuning condition
-                if self._check_finetuning_condition():
-                    self._execute_finetuning(function_description)
+                if self._check_finetuning_condition(log_file_path):
+                    self._execute_finetuning(function_description, log_file_path)
         except Exception as e:
             print(e)
             print("Error checking for finetuning")
 
-    def _check_finetuning_condition(self):
+    def _check_finetuning_condition(self, log_file_path):
         """
         Check if the finetuning condition is met
         Currently finetuning condition is dependent on the number of datapoints since last finetuning
         """
-        last_training_run_datapoints = self.config["last_training_run"]["trained_on_datapoints"]
-        return self.config["current_datapoints"] - last_training_run_datapoints > 10*(2**self.config["nr_of_training_runs"])
+        last_training_run_datapoints = self.configs[log_file_path]["last_training_run"]["trained_on_datapoints"]   
+        return self.dataset_lengths[log_file_path] - last_training_run_datapoints > 10*(2**self.configs[log_file_path]["nr_of_training_runs"])
 
-    def _execute_finetuning(self, function_description):
+    def _execute_finetuning(self, function_description, log_file_path):
         """
         Execute the finetuning
         First create the OpenAI compatible dataset with jsonL file and upload it
         Then submit the OpenAI finetuning job
         Finally update the config file to reflect the new finetuning job as current
         """
-        training_dataset = os.path.join(self._get_log_directory(), self.message)
         # read in the dataset file
-        with open (training_dataset, "rb") as f:
-            dataset = f.read().decode('utf-8').split("\r\n")
+        with open (log_file_path, "rb") as f:
+            dataset = f.read().decode('utf-8')
+        dataset.replace("\\n", "[SEP_TOKEN]")
+        dataset = dataset.split("\n")
+        dataset = [x.replace("[SEP_TOKEN]", "\\n") for x in dataset if x != ""]
         # read in the dataset file
-        dataset = [ast.literal_eval(x) for x in dataset if x != ""]
+        dataset = [ast.literal_eval(x) for x in dataset]
         #
         # create the openai dataset
         instruction = "Optionally convert the input into the output type, using the docstring as a guide. Return None if you can't."
@@ -248,51 +272,51 @@ class BufferedLogger(Logger):
 
         training_file_id = response["id"]
         finetuning_response = openai.FineTuningJob.create(training_file=training_file_id, model="gpt-3.5-turbo", suffix = "test_autom")
-        self.config["current_training_run"] = {"job_id": finetuning_response["id"],
-                                                "trained_on_datapoints": self.config["current_datapoints"],
+        self.configs[log_file_path]["current_training_run"] = {"job_id": finetuning_response["id"],
+                                                "trained_on_datapoints": self.configs[log_file_path]["current_datapoints"],
                                                 "last_checked": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
         # update the config json file
         try:
-            self._update_config_file()
+            self._update_config_file(log_file_path)
         except Exception as e:
             print(e)
             print("Could not update config file to register a finetuning run")
 
 
-    def _check_finetuning_status(self):
+    def _check_finetuning_status(self, log_file_path):
         """
         Check the status of the current finetuning job
         If the job is finished, update the config file to reflect the new model
         """
 
-        job_id = self.config["current_training_run"]["job_id"]
-        last_checked = self.config["current_training_run"]["last_checked"]
+        job_id = self.configs[log_file_path]["current_training_run"]["job_id"]
+        last_checked = self.configs[log_file_path]["current_training_run"]["last_checked"]
         # check if last checked was more than 30 mins ago
         if (datetime.datetime.now() - datetime.datetime.strptime(last_checked, "%Y-%m-%d %H:%M:%S")).total_seconds() > 1800:
             response = openai.FineTuningJob.retrieve(job_id)
-            self.config["current_training_run"]["last_checked"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.configs[log_file_path]["current_training_run"]["last_checked"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             if response["status"] == "succeeded":
-                self._update_finetune_config(response)
+                self._update_finetune_config(response, log_file_path)
             else: 
-                self._update_config_file()
+                self._update_config_file(log_file_path)
 
-    def _update_finetune_config(self, response):
+    def _update_finetune_config(self, response, log_file_path):
         """
         Update the config file to reflect the new model and switch the current model to the finetuned model
         """
-        self.config["current_model"] = response["fine_tuned_model"]
-        self.config["last_training_run"] = self.config["current_training_run"]
-        self.config["current_model_stats"] = {"trained_on_datapoints": self.config["current_training_run"]["trained_on_datapoints"], "running_faults": []}
-        self.config["nr_of_training_runs"] += 1
-        self.config["current_training_run"] = {}
+        self.configs[log_file_path]["current_model"] = response["fine_tuned_model"]
+        self.configs[log_file_path]["last_training_run"] = self.configs[log_file_path]["current_training_run"]
+        self.configs[log_file_path]["current_model_stats"] = {"trained_on_datapoints": self.configs[log_file_path]["current_training_run"]["trained_on_datapoints"], "running_faults": []}
+        self.configs[log_file_path]["nr_of_training_runs"] += 1
+        self.configs[log_file_path]["current_training_run"] = {}
         try:
-            self._update_config_file()
+            self._update_config_file(log_file_path)
         except Exception as e:
             print(e)
             print("Could not update config file after a successful finetuning run")
             pass
 
-    def _update_datapoint_config(self, priority):
+    def _update_datapoint_config(self, priority, log_file_path):
         """
         Update the config to reflect the new datapoint in the training data
         First adds 1 to the current datapoints
@@ -303,29 +327,29 @@ class BufferedLogger(Logger):
            priority (bool): whether the datapoint was fixed by the teacher model/should be added to the training data
         """
         try:
-            self.config["current_datapoints"] += 1
             if priority:
-                self.config["current_model_stats"]["running_faults"].append(1)
+                self.configs[log_file_path]["current_model_stats"]["running_faults"].append(1)
             else:
-                self.config["current_model_stats"]["running_faults"].append(0)
+                self.configs[log_file_path]["current_model_stats"]["running_faults"].append(0)
             # take the last 100 datapoints
-            self.config["current_model_stats"]["running_faults"] = self.config["current_model_stats"]["running_faults"][-100:]
+            self.configs[log_file_path]["current_model_stats"]["running_faults"] = self.configs[log_file_path]["current_model_stats"]["running_faults"][-100:]
 
             # check if the last 10 datapoints are 50% faulty, this is the switch condition
-            if sum(self.config["current_model_stats"]["running_faults"][-10:])/10 > 0.5:
-                self.config["current_model"] = self.config["teacher_models"][0]
-                self.config["current_model_stats"]["trained_on_datapoints"] = 0
-                self.config["current_model_stats"]["running_faults"] = []
+            if sum(self.configs[log_file_path]["current_model_stats"]["running_faults"][-10:])/10 > 0.5:
+                self.configs[log_file_path]["current_model"] = self.configs[log_file_path]["teacher_models"][0]
+                self.configs[log_file_path]["current_model_stats"]["trained_on_datapoints"] = 0
+                self.configs[log_file_path]["current_model_stats"]["running_faults"] = []
+            self._update_config_file(log_file_path)
 
-            self._update_config_file()
         except Exception as e:
             print(e)
             print("Could not update config file")
             pass
 
-    def _update_config_file(self):
+    def _update_config_file(self, log_file_path):
         """
         Update the config file with the new config
         """
-        with open(os.path.join(self._get_log_directory(), f"{self.message}.json"), "w") as f:
-                json.dump(self.config, f)
+        config_to_be_saved = self.configs[log_file_path]
+        with open(f"{log_file_path}.json", "w") as f:
+                json.dump(config_to_be_saved, f)
