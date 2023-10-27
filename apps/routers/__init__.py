67e5a89a678e8e95f3f1300aa5231cb3c0f30dc0
MichaelSel <seltenmusic@gmail.com>
2023-10-27 17:33:25 -0400

    POC Food review ratings

--- apps/routers/__init__.py
+++ apps/routers/__init__.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter
 # local
 from routers import (
     youtube_router as youtube,
+    food_router as food,
 )
 
 
 router = APIRouter()
 router.include_router(youtube.router)
+router.include_router(food.router)
---
+++ apps/routers/food_router.py
@@ -0,0 +1,26 @@
+# standard / third party
+import json
+
+from fastapi import APIRouter
+
+# local
+from controllers.food_controller import get_best_dishes, get_ratings
+
+from services.food_service import get_yelp_reviews
+
+router = APIRouter(
+    prefix="/food",
+    tags=["food"],
+)
+
+
+@router.get("/")
+async def analyze_reviews(url: str) -> dict[str, str]:
+    reviews = get_yelp_reviews(url)
+    ratings = get_ratings(reviews)
+    best_dishes = get_best_dishes(reviews)
+    return {
+        "message": "success",
+        "ratings": ratings,
+        "best_dishes": best_dishes,
+    }
