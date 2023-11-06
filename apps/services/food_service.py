827f41735c3ca25330b82f2325f6e8136286067b
MichaelSel <seltenmusic@gmail.com>
2023-11-06 14:01:17 -0500

    Using Proxy to scrape like a MOFO

--- apps/services/food_service.py
+++ apps/services/food_service.py
@@ -15,6 +15,11 @@ from extenders.yelp_scraper import yelp_scraper
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+from zenrows import ZenRowsClient
+
+client = ZenRowsClient(os.getenv("ZENROWS_API_KEY"))
+
+
 class RatingModel(BaseModel):
     food: int = Annotated[
         Field(..., ge=1, le=10), "The food-only rating based on the provided reviews"
@@ -61,21 +66,26 @@ def test_recommended_dishes():
     """We can test the function as normal using Pytest or Unittest"""
 
 
-def get_yelp_reviews(url: str) -> List[str]:
-    # url = f"https://api.yelp.com/v3/businesses/{business_id_or_alias}/reviews?offset=100&limit=20&sort_by=yelp_sort"
-    # headers = {
-    #     "accept": "application/json",
-    #     "Authorization": f"Bearer {os.getenv('YELP_API_KEY')}",
-    # }
-    # response = requests.get(url, headers=headers)
-    # data = response.json()
-    # reviews = [review["text"] for review in data["reviews"]]
-    # url = f"https://api.yelp.com/v3/businesses/{business_id_or_alias}/review_highlights"
-    # response = requests.get(url, headers=headers)
-    # data = response.json()
-
-    data = yelp_scraper(url)
-    reviews = [entry["reviews"] for entry in data if "reviews" in entry.keys()]
-    reviews = [item["text"] for item in reviews[0]]
+def get_yelp_reviews(business_id_or_alias: str) -> List[str]:
+    url = f"https://www.yelp.com/biz/{business_id_or_alias}"
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Host": "httpbin.org",
+        "Sec-Ch-Ua": '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    }
+    params = {"autoparse": "true"}
+    response = client.get(url, params=params)
+    res_json = response.json()["review"]
+    reviews = [review["description"] for review in res_json]
 
     return reviews
