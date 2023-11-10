c38c298a104ef746e67f7303b26c583cb32ba485
MartBakler <113173969+MartBakler@users.noreply.github.com>
2023-11-10 16:45:54 +0000

    Feature/smarter align loading (#54)
    Now loading align statements and datasets for your specific function during startup, not for all functions. Also have the possibility to turn off finetuning model loading from openai, furthermore reducing latency during startup/first call

--- src/monkey_patch/monkey.py
+++ src/monkey_patch/monkey.py
@@ -74,8 +74,8 @@ class Monkey:
 
 
     @staticmethod
-    def _load_alignments():
-        Monkey.function_modeler.load_align_statements()
+    def _load_alignments(func_hash: str):
+        Monkey.function_modeler.load_align_statements(func_hash)
 
     @staticmethod
     def align(test_func):
@@ -198,7 +198,8 @@ class Monkey:
 
     @staticmethod
     def patch(test_func):
-        Monkey._load_alignments()
+        function_description = Register.load_function_description(test_func)
+        Monkey._load_alignments(function_description.__hash__())
 
         @wraps(test_func)
         def wrapper(*args, **kwargs):
@@ -244,4 +245,6 @@ class Monkey:
     def configure(**kwargs):
         if "workspace_id" in kwargs:
             Monkey.function_modeler.workspace_id = kwargs["workspace_id"]
+        if "check_for_finetunes" in kwargs:
+            Monkey.function_modeler.check_for_finetunes = kwargs["check_for_finetunes"]
             
