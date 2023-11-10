514e9566949cd9af8ea8c2fb0fdbd1c9dddd322d
MichaelSel <seltenmusic@gmail.com>
2023-11-10 14:46:25 -0500

    More verbose execution

--- apps/routers/food_router.py
+++ apps/routers/food_router.py
@@ -16,16 +16,23 @@ router = APIRouter(
 
 @router.get("/")
 async def analyze_reviews(url: str):
+    print("Got ALIAS:", url)
+    print("Fetching reviews")
     reviews = get_yelp_reviews(url)
+    print("Got reviews")
+
     max_reviews = 20
     if len(reviews) == 0:
         return {
             "message": "error",
         }
+    print("Here is the first review:")
+    print(reviews[0])
     if len(reviews) > max_reviews:
         reviews = reviews[:max_reviews]
-
+    print("Getting ratings")
     ratings = get_ratings(reviews)
+    print("Got ratings")
     # best_dishes = get_best_dishes(reviews)
     return {
         "message": "success",
