eb0af70e2973ea1272c31a81ce6f79ccf74c0155
Mart <mart@paperplane.ai>
2023-11-14 10:56:40 +0200

    updated linting

--- examples/twitter_support/main.py
+++ examples/twitter_support/main.py
@@ -32,13 +32,25 @@ def classify_and_respond(tweet_text: str) -> Response:
 @monkey.align
 def align_respond():
     input_tweet_1 = "Laia Johnson: I really like the new shovel but the handle broke after 2 days of use. Can I get a replacement?"
-    assert classify_and_respond(input_tweet_1) == Response(requires_ticket=True, response="Hi, we are sorry to hear that. We will get back to you with a replacement as soon as possible, can you send us your order nr?")
+    assert classify_and_respond(input_tweet_1) == Response(
+                                                            requires_ticket=True, 
+                                                            response="Hi, we are sorry to hear that. We will get back to you with a replacement as soon as possible, can you send us your order nr?"
+                                                            )
     input_tweet_2 = "Keira Townsend: I hate the new design of the iphone. It is so ugly. I am switching to Samsung"
-    assert classify_and_respond(input_tweet_2) == Response(requires_ticket=False, response="Hi, we are sorry to hear that. We will take this into consideration and let the product team know of the feedback")
+    assert classify_and_respond(input_tweet_2) == Response(
+                                                            requires_ticket=False, 
+                                                            response="Hi, we are sorry to hear that. We will take this into consideration and let the product team know of the feedback"
+                                                            )
     input_tweet_3 = "Thomas Bell: @Amazonsupport. I have a question about ordering, do you deliver to Finland?"
-    assert classify_and_respond(input_tweet_3) == Response(requires_ticket=True, response="Hi, thanks for reaching out. The question will be sent to our support team and they will get back to you as soon as possible")
+    assert classify_and_respond(input_tweet_3) == Response(
+                                                            requires_ticket=True, 
+                                                            response="Hi, thanks for reaching out. The question will be sent to our support team and they will get back to you as soon as possible"
+                                                            )
     input_tweet_4 = "Jillian Murphy: Just bought the new goodybox and so far I'm loving it!"
-    assert classify_and_respond(input_tweet_4) == Response(requires_ticket=False, response="Hi, thanks for reaching out. We are happy to hear that you are enjoying the product")
+    assert classify_and_respond(input_tweet_4) == Response(
+                                                            requires_ticket=False, 
+                                                            response="Hi, thanks for reaching out. We are happy to hear that you are enjoying the product"
+                                                            )
 
 @monkey.patch
 def create_support_ticket(tweet_text: str) -> SupportTicket:
@@ -49,11 +61,23 @@ def create_support_ticket(tweet_text: str) -> SupportTicket:
 @monkey.align
 def align_supportticket():
     input_tweet_1 = "Laia Johnson: I really like the new shovel but the handle broke after 2 days of use. Can I get a replacement?"
-    assert create_support_ticket(input_tweet_1) == SupportTicket(name = "Laia Johnson", issue="Needs a replacement product because the handle broke", urgency = "high")
+    assert create_support_ticket(input_tweet_1) == SupportTicket(
+                                                                name = "Laia Johnson", 
+                                                                issue="Needs a replacement product because the handle broke", 
+                                                                urgency = "high"
+                                                                )
     input_tweet_2 = "Thomas Bell: @Amazonsupport. I have a question about ordering, do you deliver to Finland?"
-    assert create_support_ticket(input_tweet_2) == SupportTicket(name="Thomas Bell", issue="Answer whether we deliver to Finland", urgency="low")
+    assert create_support_ticket(input_tweet_2) == SupportTicket(
+                                                                name="Thomas Bell", 
+                                                                issue="Answer whether we deliver to Finland", 
+                                                                urgency="low"
+                                                                )
     input_tweet_3 = "Jillian Murphy: Just bought the new goodybox and so far I'm loving it! The cream package was slightly damaged however, would need that to be replaced"
-    assert classify_and_respond(input_tweet_3) == SupportTicket(name="Jillian Murphy", issue="Needs a new cream as package was slightly damaged", urgency="medium")
+    assert classify_and_respond(input_tweet_3) == SupportTicket(
+                                                                name="Jillian Murphy", 
+                                                                issue="Needs a new cream as package was slightly damaged", 
+                                                                urgency="medium"
+                                                                )
 
 def main():
     """
