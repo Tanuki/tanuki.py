32ecdd39406758708a56d0fec8b311cd6b4f3ee8
MichaelSel <seltenmusic@gmail.com>
2023-11-15 16:07:43 -0500

    Added marketplace classifier example

---
+++ examples/marketplace_classifier/README.md
@@ -0,0 +1,125 @@
+# Marketplace Product Auto-Tagger using Monkey-Patch 🙈 
+
+Example of how Monkey-Patch can be used to auto-tag products based on their description and name for a marketplace app.
+
+## Installation
+
+1. Install dependencies from requirements.txt
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Create a `.env` file based on the `.env.example` file and fill in the required information. 
+
+**Note**: You'll need an [OpenAI API key](https://openai.com/blog/openai-api) to use this. *Also note*, this demo uses the [BestBuy Product API](https://bestbuyapis.github.io/api-documentation/#products-api) , but you can use any API you want. Just make sure to update the code accordingly. If you want to use BestBuy, you'll need to create an account and get an API key from them.
+
+3. Run the app
+
+```bash
+python app.py
+```
+
+## Examples
+
+Original input:
+
+```
+Product: Comma [LP] - VINYL
+Short Description: None
+Description: None
+Long Description: None
+```
+    
+
+Generated output:
+```python
+{
+    'category': 'Music & Audio', 
+    'brand': 'unknown', 
+    'features': 'VINYL'
+}
+```
+
+<hr />
+
+Original input:
+
+```
+    Product: 4-Year Standard Geek Squad Protection
+    
+    Short Description: Enhance your manufacturer warranty and get extended coverage when the warranty ends, including:
+    If your original bulb burns out, we'll replace it. If there's a failure from normal wear and tear, we'll repair it. 
+    If your projector won't work because of a power surge, we'll fix it. You'll never pay for parts and labor on covered 
+    repairs.
+    
+    Description: None
+    
+    Long Description: You're excited to buy a new projector for your home &#8212; now, make sure to protect it. Geek 
+    Squad&#174; Protection enhances your manufacturer warranty and gives you extended coverage when the warranty ends.  
+    Geek Squad Protection often covers important repairs that your manufacturer warranty doesn't (like failure due to a 
+    power surge).  If something goes wrong after the manufacturer warranty ends, rest easy; you won't be stuck with a 
+    huge repair bill. Geek Squad Protection has your back and will take care of any covered repairs.
+```
+    
+
+Generated output:
+```python
+{
+    'category': 'Electronics', 
+    'brand': 'Geek Squad', 
+    'features': 'Extended warranty, Coverage for bulb burnout, Coverage for normal wear and tear, Coverage for power surge'
+}
+
+```
+
+<hr />
+
+```
+    Product: Linon Home Décor - Kessler Two-Tone Childrens Bookcase - White and Natural
+    
+    Short Description: None
+    
+    Description: None
+    
+    Long Description: Keeping your little one&#8217;s play space organized is a breeze with this bookcase. A fun and 
+    functional farmhouse look will encourage your child to keep their space tidy. An ample top surface, an upper shelf, 
+    and a lower bin accented with a fence design are perfectly sized to fit all your child&#8217;s books, stuffed 
+    animals, and toys. The combination of a white finish with natural pine wood accents will combine well with any 
+    d&#233;cor. Solidly constructed square peg legs provide support on any flat surface.
+```
+    
+
+Generated output:
+```python
+{
+    'category': 'Home Goods', 
+    'brand': 'Linon Home Décor', 
+    'features': 'Two-Tone, White and Natural, upper shelf, lower bin, white finish with natural pine wood accents, Solidly constructed square peg legs'
+}
+```
+
+
+<hr />
+
+```
+    Product: Razer - Kraken Kitty Edition V2 Pro Wired Gaming Headset - Black
+    
+    Short Description: None
+    
+    Description: None
+    
+    Long Description: Creating the cutest stream persona now comes in more ways than one. Switch up your style and light 
+    up your stream with a Razer Chroma RGB headset featuring 3 interchangeable ear designs, stream reactive lighting for 
+    next-level audience engagement, and a solid mic for crystal-clear voice capture.
+```
+    
+
+Generated output:
+```python
+{
+    'category': 'Electronics', 
+    'brand': 'Razer', 
+    'features': 'Razer Chroma RGB, interchangeable ear designs, stream reactive lighting, solid mic for crystal-clear voice capture'
+}
+```
---
+++ examples/marketplace_classifier/app.py
@@ -0,0 +1,106 @@
+import os
+
+# Load in the environment variables
+import dotenv
+dotenv.load_dotenv()
+
+# Initialize OpenAI
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY") # Your OpenAI API key
+
+# import Monkey Patch 🙈
+from monkey_patch.monkey import Monkey
+
+# import Pydantic to define the response type
+from pydantic import Field, BaseModel
+
+# Additional imports for the demo that will be used to fetch a random product from the Best Buy API
+from random import randint
+import requests
+
+
+# First we define the desired response type using Pydantic
+class ProductTags(BaseModel):
+    category: str = Field(..., description="What category does this product fit into?")
+    brand: str = Field(..., description="What brand is this product?")
+    features: str = Field(..., description="What are the features of this product?")
+
+
+# Next we define the function that will be used to generate the response with the Monkey Patch decorator
+@Monkey.patch
+def product_tagger(product_story: str) -> ProductTags:
+    """
+    based on the provided product story, extract the following aspects:
+    - what category does this product fit into? (e.g., Sports & outdoors, Home Goods, Mens Clothing, Garden Tools, etc.)
+    - what brand is this product? (e.g., Nike, Adidas, Apple, Samsung, etc.)
+    - what are the features of this product? (e.g., Bluetooth, 4K, 5G, water-resistant, removable liner, etc.)
+    If it is not possible to extract any of these aspects, return "unknown" for that aspect.
+    """
+
+# (OPTIONAL!) We use the align decorator to both test the function, and to teach the model about our expected output.
+# This can include as many or as few assert statements as you like.
+@Monkey.align
+def test_product_tagger():
+    """We can test the function as normal using Pytest or Unittest"""
+    assert product_tagger("Product: Apple iPhone 12 Pro Max 5G 128GB - Pacific Blue (Verizon) Short Description: "
+                          "iPhone 12 Pro Max. 5G to download movies on the fly and stream high-quality video.¹ "
+                          "Beautifully bright 6.7-inch Super Retina XDR display.² Ceramic Shield with 4x better drop "
+                          "performance.³ Incredible low-light photography with the best Pro camera system on an "
+                          "iPhone, and 5x optical zoom range. Cinema-grade Dolby Vision video recording, editing, "
+                          "and playback. Night mode portraits and next-level AR experiences with the LiDAR Scanner. "
+                          "Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless "
+                          "charging.⁴ For infinitely spectacular possibilities. Features: 6.7-inch Super Retina XDR "
+                          "display Ceramic Shield, tougher than any smartphone glass A14 Bionic chip, the fastest "
+                          "chip ever in a smartphone Pro camera system with 12MP Ultra Wide, Wide, and Telephoto "
+                          "cameras; 5x optical zoom range; Night mode, Deep Fusion, Smart HDR 3, Apple ProRAW, "
+                          "4K Dolby Vision HDR recording LiDAR Scanner for improved AR experiences, Night mode "
+                          "portraits 12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording "
+                          "Industry-leading IP68 water resistance Supports MagSafe accessories for easy attach and "
+                          "faster wireless charging iOS with redesigned widgets on the Home screen, all-new App "
+                          "Library, App Clips and more") == ProductTags(
+        category='Electronics',
+        brand='Apple',
+        features='5G, 4K, Dolby Vision, LiDAR Scanner, Night mode, 12MP TrueDepth front camera, IP68 water '
+                 'resistance, MagSafe accessories, iOS'
+    )
+
+
+# Finally we can run the function on a random product from the Best Buy API
+if __name__ == '__main__':
+
+    # get the Best Buy API key from the environment variables (this is set in .env file)
+    bestbuy_api_key = os.getenv("BESTBUY_API")
+
+    # get random number between 1 and 160000 (roughly the number of products in the Best Buy API)
+    random_number = randint(1, 160000)
+
+    # set the Best Buy API endpoint
+    bestbuy_api = "https://api.bestbuy.com/v1/products"
+
+    # fetch a random product from the API
+    response = requests.get(f"{bestbuy_api}?apiKey={bestbuy_api_key}&pageSize=1&page={random_number}&format=json")
+    response.raise_for_status()
+
+    # get the product from the response (if no errors were raised)
+    product = response.json()['products'][0]
+
+    # Let's assemble the input for the product tagger
+    product_story = f"""
+    Product: {product['name']}
+    Short Description: {product['shortDescription']}
+    Description: {product['description']}
+    Long Description: {product['longDescription']}
+    """
+
+    # (Optional) Run the test to ensure the function works as expected and to teach the model about the expected output
+    test_product_tagger()
+
+    # Run the function on the product story and show the results
+    print("\nOriginal input:")
+    print(product_story)
+    print("\nGenerated output:")
+
+    tagged_product = product_tagger(product_story)
+
+    # we can safely print the model dump because we know its type is ProductTags
+    print(tagged_product.model_dump())
