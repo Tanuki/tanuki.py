a3af65aa5ffbe3128044daa5d75a2a4edcf857e4
Jack Hopkins <jack.hopkins@me.com>
2023-11-28 09:51:40 +0000

    Fix/versions (#104)
    * Commit subject: Fix versions in project files
    
    Commit description:
    
    * Commit subject line: Update version to 0.1.0
    
    Commit description:
    
    * Commit subject line: Fix package path in setup.py
    
    Description: This commit fixes the package path in the setup.py file. Previously, the packages were defined as 'src/tanuki', but it has been changed to 'tanuki' to match the package name. Additionally, the version has been updated to '0.1.0'.
    
    ---------
    
    Co-authored-by: Jack Hopkins <jack@raveler.co.uk>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='tanuki.py',  # How you named your package folder (MyLib)
-    packages=['src/tanuki'],  # Chose the same as "name"
+    packages=['tanuki'],  # Chose the same as "name"
     version='0.1.0',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='The easiest way to build scalable LLM-powered applications, which gets cheaper and faster over time.',
