1aa943efc8b04fce21d5ef263709e52f5d0ae00b
Mart <mart@paperplane.ai>
2024-01-30 15:21:55 +0000

    added new models

--- src/tanuki/language_models/llm_configs/__init__.py
+++ src/tanuki/language_models/llm_configs/__init__.py
@@ -15,11 +15,16 @@ DEFAULT_TEACHER_MODELS = {
                                         context_length = 128000,
                                         instructions="You are given below a function description and input data. The function description of what the function must carry out can be found in the Function section, with input and output type hints. The input data can be found in Input section. Using the function description, apply the function to the Input and return a valid output type, that is acceptable by the output_class_definition and output_class_hint.\nINCREDIBLY IMPORTANT: Only output a JSON-compatible string in the correct response format. Use the [END] tokens to specify when the output ends.",
                                         parsing_helper_tokens={"start_token": "[START]", "end_token": "[END]"}),
-            "Mixtral-8x7B": TogetherAIConfig(model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1",
-                                        context_length = 16800),
             "anthropic.claude-v2:1": ClaudeConfig(model_name = "anthropic.claude-v2:1", context_length = 200000),
             "llama_70b_chat_aws": LlamaBedrockConfig(model_name = "meta.llama2-70b-chat-v1", context_length = 4096),
             "llama_13b_chat_aws": LlamaBedrockConfig(model_name = "meta.llama2-13b-chat-v1", context_length = 4096),
+            "Mixtral-8x7B": TogetherAIConfig(model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1",
+                                            chat_template = "{user_prompt}", # for some reason this worked better than using their own supplied chat template
+                                            context_length = 32768),
+            "OpenHermes-2p5-Mistral": TogetherAIConfig(model_name = "teknium/OpenHermes-2p5-Mistral-7B",
+                                            context_length = 4096),
+            "llama13b-togetherai": TogetherAIConfig(model_name = "togethercomputer/llama-2-13b-chat",
+                                            context_length = 4096),
         }
 
 DEFAULT_STUDENT_MODELS = {
